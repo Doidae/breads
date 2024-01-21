8a22d654e83e0687019b6cd59ceb566b6595c8d4
Doidae <andyarredondo1224@gmail.com>
2024-01-21 11:43:42 -0500

    added routes and listener

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const express = require('express')
 //Configuration
 require('dotenv').config()
 const PORT = process.env.PORT
-console.log(PORT)
+const app = express()
+
+//Routes
+app.get('/', (req,res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+//Listen
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
