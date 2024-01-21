757d51bfd2c9ed4cc70df8d111af3f0add3554a0
Doidae <andyarredondo1224@gmail.com>
2024-01-21 11:59:27 -0500

    Added the breads route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', (req,res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
+//Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
 //Listen
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
